dd3c241c73884e1c46236929533b86dc6811b38a
John Papa <john@johnpapa.net>
2015-09-21 23:00:09 -0400

    made service cache the characters.
    renamed hero to character

--- src/app/core/character.service.ts
+++ src/app/core/character.service.ts
@@ -4,33 +4,33 @@ import {Injectable} from 'angular2/angular2';
 
 @Injectable()
 export class CharacterService {
+	characters: Character[] = [];
+
 	constructor(private _http: Http) {}
 
 	getCharacters(): Rx.IPromise<Character[]> {
+		this.characters.length = 0;
 		let promise = this._http.get('characters.json')
-			.toRx().map((response: any) => response.json()).toPromise();
+			.toRx().map((response: any) => response.json()).toPromise()
+			.then(characters => {
+				this.characters.push(...characters);
+				return this.characters;
+			})
+			//TODO: fix catch
+			//.catch(e => this._fetchFailed(e)) // want we want to say
+			// baroque way to ensure promise stays Promise<Hero[]>
+			.then<Character[]>(_ => _, e => this._fetchFailed(e));
 		return promise;
 	}
 
-    getCharacter(id: number) {
-        let heroes: string[];
-        heroes.length = 0;
+	private _fetchFailed(error:any) {
+		console.error(error);
+		return Promise.reject(error);
+	}
+
+	getCharacter(id: number) {
 		return this.getCharacters().then((characters) => { return characters.filter((c) => {
 			return c.id === id;
 		})[0]});
 	}
 }
-	// getCharacters(id: number) {
-	// 	return getCharacters().then((characters) => { return characters.filter((c) => {
-	// 			return c.id === id;
-	// 		})[0]});
-	// }
-
-	// getCharacters() { return Promise.resolve(CHARACTERS); }
-
-	// getCharacter(id: number) {
-	// 	return Promise.resolve(CHARACTERS)
-	// 		.then((characters) => { return characters.filter((c) => {
-	// 			return c.id === id;
-	// 		})[0]});
-	// }
